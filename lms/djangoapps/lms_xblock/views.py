be7dab89a41ddfd2c8f956a256d5b7a959415957
Dipankar Dey <dipankar.dey@learningmate.com>
2023-06-13 19:28:54 +0530

    fix for notification API

--- lms/djangoapps/lms_xblock/views.py
+++ lms/djangoapps/lms_xblock/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import CreateAPIView,ListAPIView,APIView
+from rest_framework.generics import CreateAPIView,ListAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
