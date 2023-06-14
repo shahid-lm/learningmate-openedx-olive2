b480441350ec053daa51e1e4a5336ed0e4a1f5ff
Dipankar Dey <dipankar.dey@learningmate.com>
2023-06-15 00:28:55 +0530

    fix for sga delete endpoint

--- lms/djangoapps/lms_xblock/urls.py
+++ lms/djangoapps/lms_xblock/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('sgasubmission', views.StaffGradedSubmissionsView.as_view(), name="sgasubmission"),
-    path('delete_sgasubmission/<int:id>', views.StaffGradedSubmissionsView.as_view(), name="delete_sgasubmission"),
+    path('delete_sgasubmission/<int:id>', views.DeleteStaffGradedSubmissions.as_view(), name="delete_sgasubmission"),
 
 ]
